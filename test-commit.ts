9cc7414d57043272ed6ebfe5dd36d73ae4e7d4bd
Ivan Dachev <i_dachev@yahoo.co.uk>
2025-07-16 07:06:53 +0300

    test: checking git hooks

---
+++ test-commit.ts
@@ -0,0 +1,6 @@
+// This is a test file for checking git hooks
+export const test = 'hello world';
+
+const _unusedVariable = 'this is allowed with underscore prefix';
+
+console.warn('This is allowed by our rules');
